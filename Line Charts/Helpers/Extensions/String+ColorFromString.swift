5c7c09fb3ec24a322a1b1d66d29beeeb7bbe8460
LackyUA <ddmutro2@gmail.com>
2019-03-26 17:11:10 +0200

    Refactored some functions, changed line adding and removing logic. Added ChartPoint model. Some code refactored.

---
+++ Line Charts/Helpers/Extensions/String+ColorFromString.swift
@@ -0,0 +1,9 @@
+//
+//  String+ColorFromString.swift
+//  Line Charts
+//
+//  Created by Dmytro Dobrovolskyy on 3/25/19.
+//  Copyright © 2019 LackyUA. All rights reserved.
+//
+
+import Foundation
